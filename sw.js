733bf34a5bed2b4cae697cd10bf4fde50e958f67
dingbaiyun308-create <dingbaiyun308@gmail.com>
2026-08-04 18:58:30 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'training-plan-phase3-v20260804-3';
+const CACHE_NAME = 'training-plan-phase3-v20260804-4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/apple-touch-icon.png', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
